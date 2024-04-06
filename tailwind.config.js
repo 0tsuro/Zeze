9acceeba4940b582535f648d95cbc92d266af958
0tsuro <160429713+0tsuro@users.noreply.github.com>
2024-04-06 21:41:06 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'regal-blue': '#7631A6',
+      },
+    }
+  }
+}
